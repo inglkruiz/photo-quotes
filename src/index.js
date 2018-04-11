abdf16e5b136349ca787f0b450212fb0f24efbf6
inglkruiz <ing.lkruiz@gmail.com>
2018-04-11 11:03:02 -0600

    Unsplash API Guidelines
    * Trigger a download event in order to meet new guidelines.

--- src/index.js
+++ src/index.js
@@ -25,14 +25,24 @@ function getPhotoQuote() {
     })
     .then(res => res.json()),
   ]).then((responses) => {
-    const image  = responses[0],
-      text = responses[1],
-      authorEl = document.getElementsByClassName('Quote-author')[0],
-      photographerEl = document.getElementsByClassName('App-photographer')[0],
-      { user: {name: photographerName, links: { html: photographerLink } } } = image,
-      utmParams = 'utm_source=inglkruiz&utm_medium=referral&utm_campaign=api-credit';
+    const image  = responses[0];
+    const  text = responses[1];
+    const authorEl = document.getElementsByClassName('Quote-author')[0]
+    const photographerEl = document.getElementsByClassName('App-photographer')[0];
+    const { user: {name: photographerName, links: { html: photographerLink } } } = image;
+    const utmParams = 'utm_source=inglkruiz&utm_medium=referral&utm_campaign=api-credit';
     
     document.getElementsByTagName('body')[0].style.backgroundImage = `url("${image.urls.regular}")`;
+
+    fetch(image.links.download_location, {
+      method: 'GET',
+      headers: {
+        'Accept-Version': 'v1',
+        Authorization: `Client-ID a04c2b5488ec14158ffea259aeba4a953a1609529953d7e2ec5842534a62b025`,
+      }
+    })
+
+
     document.getElementsByClassName('Quote-text')[0].innerHTML = `"${text.quote}"`;
     authorEl.setAttribute('title', text.author);
     authorEl.innerHTML = text.author;
@@ -59,6 +69,5 @@ const DOMReadyPromise = (function promise() {
 
 DOMReadyPromise.then(() => {
   getPhotoQuote();
-  // const getQuoteBtn = document.getElementById('getQuote');
   document.getElementById('getQuoteBtn').addEventListener('click', getPhotoQuote);
 })
